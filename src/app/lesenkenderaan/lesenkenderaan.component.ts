3368a8b67d42e8d136562b9763a92a12f6587192
sitinurfatinatiqah <2022862112@student.uitm.edu.my>
2025-03-24 12:10:54 +0800

    v1.0

--- src/app/lesenkenderaan/lesenkenderaan.component.ts
+++ src/app/lesenkenderaan/lesenkenderaan.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lesenkenderaan',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './lesenkenderaan.component.html',
   styleUrl: './lesenkenderaan.component.css'
+  
 })
-export class LesenkenderaanComponent {
+export class LesenkenderaanComponent implements OnInit {
+  constructor(private router: Router) {}
+  selectedVehicle: string = '';
+  generatedDate: string = '';
+  vehicleInfoVisible: boolean = false;
+
+  ngOnInit(): void {
+    this.generatedDate = this.formatDate(new Date());
+  }
+
+  toggleVehicleInfo() {
+    this.vehicleInfoVisible = !!this.selectedVehicle;  // Show if selectedVehicle has a value
+  }
+
+  reloadPage() {
+    location.reload();
+  }
+
+  formatDate(date: Date): string {
+    const months = [
+      "Januari", "Februari", "Mac", "April", "Mei", "Jun",
+      "Julai", "Ogos", "September", "Oktober", "November", "Disember"
+    ];
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  }
+
+  switchTab(index: number) {
+    if (index === 0) {
+      window.location.href = "lesen_memandu.html";
+    } else if (index === 2) {
+      this.router.navigate(['/pemilikan']);;
+    }
+  }
 
 }
